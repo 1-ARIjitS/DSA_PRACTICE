9a8cffd6f192942f49cebbf0668d19f626dafaf5
1-ARIjitS <arijits19@iiserb.ac.in>
2022-06-13 15:29:36 +0530

    max product subarray added

--- Q23 max product subarray/main.cpp
+++ Q23 max product subarray/main.cpp
@@ -1,33 +1,44 @@
 #include <iostream>
 
 using namespace std;
-
-int maxpdtsubarray(int a[], int n)
+//method 1- brute force approach O(n^2) time and O(1) space
+int maxpdtbrute(int a[], int n)
 {
     int maxpdt=0;
-    int maxpdt_th=1;
-    int negcount=0;
     for(int i=0;i<n;i++)
     {
-        if(a[i]<0)
+        int pdt=1;
+        for(int j=i;j<n;j++)
         {
-            negcount++;
+            pdt=pdt*a[j];
+            maxpdt=max(pdt,maxpdt);
         }
     }
-    for(int i=0;i<n;i++)
+    return maxpdt;
+}
+
+//method 2- using single pass and maintaining 4 vars. max,min,current,result in O(n) time and O(1) space
+int maxpdtsubarray(int a[], int n)
+{
+    int maxx=a[0];
+    int minn=a[0];
+    int maxpdt=a[0];
+    for(int i=1;i<n;i++)
     {
-    if(negcount%2==0)
-    {
-       //even no. of negative numbers so final positive result
-       maxpdt_th=maxpdt_th*a[i];
-       maxpdt=max(maxpdt, abs(maxpdt_th));
-    }
-    else
-    {
-       //odd no. of negative numbers so final negative result
-       maxpdt_th=maxpdt_th*a[i];
-       maxpdt=max(maxpdt, maxpdt_th);
-    }
+        int current=a[i];
+        if(a[i]==0)
+        {
+            maxx=1;
+            minn=1;
+            continue;
+        }
+        int currentmax=maxx*current;
+        int currentmin=minn*current;
+        maxx=max(currentmax,currentmin);
+        maxx=max(current,maxx);
+        minn=min(currentmax,currentmin);
+        minn=max(current,minn);
+        maxpdt=max(maxpdt,maxx);
     }
     return maxpdt;
 }
@@ -36,7 +47,8 @@ int main()
 {
     //int a[]={6, -3, -10, 0, 2};
     //int a[]={2,3,4,5,-1,0};
-    int a[]={2,3,-2,4};
+    int a[]={-2,3,0,-2,-4};
     int n= sizeof(a)/sizeof(a[0]);
+    //cout<<maxpdtbrute(a,n);
     cout<<maxpdtsubarray(a,n);
 }
