f8bace5b9e1d6450ce065a1fac25374461b03015
1-ARIjitS <arijits19@iiserb.ac.in>
2022-06-25 00:26:46 +0530

    median of an array added

---
+++ Q35 Median of 2 sorted arrays of equal size/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int median(int a[], int n)
+{
+    sort(a,a+n);
+    if(n%2==0)//even no. of elements
+    {
+        return (a[n/2]+a[n/2-1])/2;
+    }
+    else//odd no. of elements
+    {
+        return a[n/2];
+    }
+}
+
+int main()
+{
+    //int a[]={90,100,78,89,67};
+    int a[]={56,67,30,79};
+    int n=sizeof(a)/sizeof(a[0]);
+    cout<<median(a,n);
+}
