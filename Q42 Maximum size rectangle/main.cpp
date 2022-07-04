77cc0498636c9d80483ce7c92a8b639d7d0260f7
1-ARIjitS <arijits19@iiserb.ac.in>
2022-07-05 02:17:52 +0530

    max area rectangle possible in a matrix added

--- Q42 Maximum size rectangle/main.cpp
+++ Q42 Maximum size rectangle/main.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-//methpd 1- using stacks in O() time and O() space
+//method 1- using stacks in O() time and O() space
 int eachrowrectarea(int auxrect[], int n)
 {
     int res=0;//result to be returned stores the area of max rect in each row
