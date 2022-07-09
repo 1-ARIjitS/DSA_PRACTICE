335a775dc1f5885ec6709c5e7ab6ba0fdcc145b5
1-ARIjitS <arijits19@iiserb.ac.in>
2022-07-10 02:46:14 +0530

    reverse a string added

---
+++ Q47 Reverse a String/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+//method 1- using brute force in O(n) time and O(1) space
+void revstrbrute(string s)
+{
+    int n=s.size();
+    int i=0;
+    int j=n-1;
+    while(i<j)
+    {
+        swap(s[i],s[j]);
+        i++;
+        j--;
+    }
+    cout<<s;
+}
+
+int main()
+{
+    string s="HELLO, WHO IS THERE ?";
+    revstrbrute(s);
+}
