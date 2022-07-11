e3c88f24561898d208f7c0371fd2b0404a89ac11
1-ARIjitS <arijits19@iiserb.ac.in>
2022-07-12 00:50:18 +0530

    finding duplicates in a string added

---
+++ Q49 Find Duplicate characters in a string/main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+//method 1- using maps or unordered maps in O(n) time and O(n) space
+void dupchstrmap(string s)
+{
+    //unordered_map<char,int>m;
+    map<char,int>m;
+    int n=s.size();
+    for(int i=0;i<n;i++)
+    {
+        m[s[i]]++;
+    }
+    cout<<"duplicate characters in the given string are- "<<endl;
+    for(auto it:m)
+    {
+        if(it.second>1)
+        {
+            cout<<it.first<<" "<<"count= "<<it.second<<endl;
+        }
+    }
+}
+
+int main()
+{
+    string s="abacdefgahijjkklllllmmonbcdejghfrg";
+    dupchstrmap(s);
+}
