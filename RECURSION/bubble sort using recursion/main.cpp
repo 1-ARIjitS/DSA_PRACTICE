b9404d4dcc8c02c1f8393fd8e38a5a0b8a1bf5d8
1-ARIjitS <arijits19@iiserb.ac.in>
2022-07-19 23:41:31 +0530

    bubble sort using recursion added

---
+++ RECURSION/bubble sort using recursion/main.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+//simple bubble sort using loops
+void bubblesort(int a[], int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                swap(a[j],a[j+1]);
+            }
+        }
+    }
+}
+
+//bubble sort using recursion
+void bubblesortrec(int a[], int n)
+{
+    if(n==0 || n==1)
+    {
+        return;
+    }
+    else
+    {
+       for(int i=0;i<n;i++)
+       {
+           if(a[i+1]<a[i])
+           {
+               swap(a[i],a[i+1]);
+           }
+       }
+       bubblesort(a,n-1);
+    }
+}
+
+int main()
+{
+    int a[]={1,5,6,7,3,4,2,0,8,9};
+    int n= sizeof(a)/sizeof(a[0]);
+    bubblesortrec(a,n);
+    //bubblesort(a,n);
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+}
